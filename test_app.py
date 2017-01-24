fb314314c95cb4d7e857ea257979caeaf46d00b7
Randy Syring <randy@thesyrings.us>
2017-01-24 14:55:46 -0500

    add hacker news to view

--- test_app.py
+++ test_app.py
@@ -15,3 +15,10 @@ class TestWebViews(object):
     def test_hello_fred(self):
         resp = self.ta.get('/hello/Fred')
         assert 'Hello, Fred!' in resp
+        assert 'Hacker News' not in resp
+
+    def test_hacker_news_lookup(self):
+        resp = self.ta.get('/hello/rsyring?hnk=1')
+        assert 'Hello, rsyring!' in resp
+        assert 'Hacker News karma: 7' in resp
+
