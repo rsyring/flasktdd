38e64b2ace041d26145f18cfb6bf6e73cabb458d
Randy Syring <randy@thesyrings.us>
2017-01-24 14:42:17 -0500

    hnkarma() tests

---
+++ test_utils.py
@@ -0,0 +1,25 @@
+import requests_mock
+
+from utils import hnkarma
+
+
+class TestHackerNews(object):
+
+    def test_hacker_news_user(self):
+        user_json = {
+            "created": 1234,
+            "id": "foobar",
+            "karma": 911,
+            "submitted": [
+                567,
+                568
+            ]
+        }
+        with requests_mock.Mocker() as m:
+            m.get('https://hacker-news.firebaseio.com/v0/user/foobar.json', json=user_json)
+            assert hnkarma('foobar') == 911
+
+    def test_hacker_news_null(self):
+        with requests_mock.Mocker() as m:
+            m.get('https://hacker-news.firebaseio.com/v0/user/usernothere.json', text='null')
+            assert hnkarma('usernothere') is None
---
+++ utils.py
@@ -0,0 +1,3 @@
+
+def hnkarma(username):
+    pass
